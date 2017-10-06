539bb90211c0ac390bca7d955bdde82f2bf43ec4
Jake Beaulieu <beaulieu.jake@epa.gov>
2017-10-06 15:00:21 -0400

    FUNCTION ENHANCEMENTS
    *Added capacity to utilize separate water body and headspace equilibration temperatures.
    
    *Added 100% saturation calculation.
    
    *Updated README.Rmd accordingly

--- neonDissGas/R/def.calc.sdg.R
+++ neonDissGas/R/def.calc.sdg.R
@@ -10,15 +10,21 @@
 #' @param volGas Volume of air equilibrated with water [mL]
 #' @param volH2O Volume of water equilibrated with air [mL]
 #' @param baro Barometric pressure at the time of equilibration [kPa]
-#' @param waterTemp Temperature of the water at the time of equilibration [celsius]
+#' @param waterTemp Temperature of the waterbody when sampled [celsius]
+#' @param headspaceTemp Temperature of the water sample during the headspace equilibration [celsius]
 #' @param eqCO2 Concentration of carbon dioxide in the equilibrated gas [ppmv]
-#' @param airCO2 Concentration of carbon dioxide in the reference air [ppmv]
+#' @param airCO2 Concentration of carbon dioxide in atmosphere [ppmv]
+#' @param sourceCO2 Concentration of carbon dioxide in headspace source gas [ppmv]
 #' @param eqCH4 Concentration of methane in the equilibrated gas [ppmv]
-#' @param airCH4 Concentration of methane in the reference air [ppmv]
+#' @param airCH4 Concentration of methane in atmosphere [ppmv]
+#' @param sourceCH4 Concentration methane in headspace source gas [ppmv]
 #' @param eqN2O Concentration of nitrous oxide in the equilibrated gas [ppmv]
-#' @param airN2O Concentration of nitrous oxide in the reference air [ppmv]
+#' @param airN2O Concentration of nitrous oxide in atmosphere [ppmv]
+#' @param sourceN2O Concentration of nitrous oxide in headspace source gas [ppmv]
 
-#' @return This function returns dissolved CO2, CH4, and N2O concentrations in surface water [M] appended as additional columns to the input data frame
+#' @return This function returns dissolved CO2, CH4, and N2O concentrations in surface water [M] based on headspace equilibration data.
+#'   Function also returns dissolved 100% saturation concentrations  [M] of CO2, CH4, and N2O in surface waters.
+#'   Outputs are appended as additional columns to the input data frame
 
 #' @references
 #' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
@@ -50,17 +56,30 @@ def.calc.sdg <- function(
   volH2O = inputFile$waterVolume,
   baro = inputFile$barometricPressure,
   waterTemp = inputFile$waterTemp,
+  headspaceTemp = ifelse(is.na(inputFile$headspaceTemp), # if headspace temp not recorded, use water temp
+                  inputFile$waterTemp,
+                  inputFile$headspaceTemp),
   eqCO2 = inputFile$concentrationCO2Gas,
-  airCO2 = inputFile$concentrationCO2Air,
+  airCO2 = ifelse(is.na(inputFile$concentrationCO2Air), # use global average if not measured
+                  405, # https://www.esrl.noaa.gov/gmd/ccgg/trends/global.html
+                  inputFile$concentrationCO2Air),
+  sourceCO2 = inputFile$concentrationCO2Source,
   eqCH4 = inputFile$concentrationCH4Gas,
-  airCH4 = inputFile$concentrationCH4Air,
+  airCH4 = ifelse(is.na(inputFile$concentrationCH4Air), # use global average if not measured
+                  1.85, #https://www.esrl.noaa.gov/gmd/ccgg/trends_ch4/
+                  inputFile$concentrationCH4Air),
+  sourceCH4 = inputFile$concentrationCH4Source,
   eqN2O = inputFile$concentrationN2OGas,
-  airN2O = inputFile$concentrationN2OAir
+  airN2O = ifelse(is.na(inputFile$concentrationN2OAir), # use global average if not measured
+                  0.330, #https://www.esrl.noaa.gov/gmd/hats/combined/N2O.html
+                  inputFile$concentrationN2OAir),
+  sourceN2O = inputFile$concentrationN2OSource
 ) {
   
   if(typeof(inputFile) == "character"){
     inputFile <- read.csv(inputFile)
   }
+  
   
   ##### Constants #####
   cGas<-8.3144598 #universal gas constant (J K-1 mol-1)
@@ -68,25 +87,35 @@ def.calc.sdg <- function(
   cPresConv <- 0.000001 # Constant to convert mixing ratio from umol/mol (ppmv) to mol/mol. Unit conversions from kPa to Pa, m^3 to L, cancel out.
   cT0 <- 298.15#Henry's law constant T0
   #Henry's law constants and temperature dependence from Sander (2015) DOI: 10.5194/acp-15-4399-2015
-  ckHCO2 <- 0.00033 #mol m-3 Pa
-  ckHCH4 <- 0.000014 #mol m-3 Pa
-  ckHN2O <- 0.00024 #mol m-3 Pa
-  cdHdTCO2 <- 2400 #K
-  cdHdTCH4 <- 1900 #K
-  cdHdTN2O <- 2700 #K
+  ckHCO2 <- 0.00033 #mol m-3 Pa, range: 0.00031 - 0.00045
+  ckHCH4 <- 0.000014 #mol m-3 Pa, range: 0.0000096 - 0.000092
+  ckHN2O <- 0.00024 #mol m-3 Pa, range: 0.00018 - 0.00025
+  cdHdTCO2 <- 2400 #K, range: 2300 - 2600
+  cdHdTCH4 <- 1900 #K, range: 1400-2400
+  cdHdTN2O <- 2700 #K, range: 2600 - 3600
   
-  ##### Calculate dissolved gas concentrations #####
+  ##### Calculate dissolved gas concentration in original water sample #####
   inputFile$dissolvedCO2 <- NA
-  inputFile$dissolvedCO2 <- baro * cPresConv * (volGas*(eqCO2 - airCO2)/(cGas * (waterTemp + cKelvin) * volH2O) 
-                        + ckHCO2 * exp(cdHdTCO2*(1/(waterTemp + cKelvin) - 1/cT0))* eqCO2)
+  inputFile$dissolvedCO2 <- baro * cPresConv * (volGas*(eqCO2 - sourceCO2)/(cGas * (headspaceTemp + cKelvin) * volH2O) 
+                        + ckHCO2 * exp(cdHdTCO2*(1/(headspaceTemp + cKelvin) - 1/cT0))* eqCO2)
   
   inputFile$dissolvedCH4 <- NA
-  inputFile$dissolvedCH4 <- baro * cPresConv * (volGas*(eqCH4 - airCH4)/(cGas * (waterTemp + cKelvin) * volH2O) 
-                        + ckHCH4 * exp(cdHdTCH4*(1/(waterTemp + cKelvin) - 1/cT0))* eqCH4)
+  inputFile$dissolvedCH4 <- baro * cPresConv * (volGas*(eqCH4 - sourceCH4)/(cGas * (headspaceTemp + cKelvin) * volH2O) 
+                        + ckHCH4 * exp(cdHdTCH4*(1/(headspaceTemp + cKelvin) - 1/cT0))* eqCH4)
   
   inputFile$dissolvedN2O <- NA
-  inputFile$dissolvedN2O <- baro * cPresConv * (volGas*(eqN2O - airN2O)/(cGas * (waterTemp + cKelvin) * volH2O) 
-                        + ckHN2O * exp(cdHdTN2O*(1/(waterTemp + cKelvin) - 1/cT0))* eqN2O)
+  inputFile$dissolvedN2O <- baro * cPresConv * (volGas*(eqN2O - sourceN2O)/(cGas * (headspaceTemp + cKelvin) * volH2O) 
+                        + ckHN2O * exp(cdHdTN2O*(1/(headspaceTemp + cKelvin) - 1/cT0))* eqN2O)
+
+  
+  ##### Calculate dissolved gas concentration at 100% saturation ##### 
+  
+  # 100% saturation occurs when the dissolved gas concentration is in equilibrium
+  # with the atmosphere.
+  inputFile$satCO2 <- (ckHCO2 * exp(cdHdTCO2*(1/(waterTemp + cKelvin) - 1/cT0))) * airCO2 * baro * cPresConv
+  inputFile$satCH4 <- (ckHCH4 * exp(cdHdTCH4*(1/(waterTemp + cKelvin) - 1/cT0))) * airCH4 * baro * cPresConv  
+  inputFile$satN2O <- (ckHN2O * exp(cdHdTN2O*(1/(waterTemp + cKelvin) - 1/cT0))) * airN2O * baro * cPresConv
+  
   
   ##### Step-by-step Calculation of dissolved gas concentrations for testing #####
   
@@ -101,9 +130,9 @@ def.calc.sdg <- function(
   # balance calculation and as partial pressure for the equilibrium calculation.
   
   # #Temperature corrected Henry's Law Constant
-  # HCO2 <- ckHCO2 * exp(cdHdTCO2 * ((1/(waterTemp+cKelvin)) - (1/cT0)))
-  # HCH4 <- ckHCH4 * exp(cdHdTCH4 * ((1/(waterTemp+cKelvin)) - (1/cT0)))
-  # HN2O <- ckHN2O * exp(cdHdTN2O * ((1/(waterTemp+cKelvin)) - (1/cT0)))
+  # HCO2 <- ckHCO2 * exp(cdHdTCO2 * ((1/(headspaceTemp+cKelvin)) - (1/cT0)))
+  # HCH4 <- ckHCH4 * exp(cdHdTCH4 * ((1/(headspaceTemp+cKelvin)) - (1/cT0)))
+  # HN2O <- ckHN2O * exp(cdHdTN2O * ((1/(headspaceTemp+cKelvin)) - (1/cT0)))
   # 
   # #Mol of gas in equilibrated water (using Henry's law)
   # CO2eqWat <- HCO2 * eqCO2 * cPresConv * baro * (volH2O/1000)
@@ -111,14 +140,14 @@ def.calc.sdg <- function(
   # N2OeqWat <- HN2O * eqN2O * cPresConv * baro * (volH2O/1000)
   # 
   # #Mol of gas in equilibrated air (using ideal gas law)
-  # CO2eqAir <- (eqCO2 * cPresConv * baro * (volGas/1000))/(cGas * (waterTemp + cKelvin))
-  # CH4eqAir <- (eqCH4 * cPresConv * baro * (volGas/1000))/(cGas * (waterTemp + cKelvin))
-  # N2OeqAir <- (eqN2O * cPresConv * baro * (volGas/1000))/(cGas * (waterTemp + cKelvin))
+  # CO2eqAir <- (eqCO2 * cPresConv * baro * (volGas/1000))/(cGas * (headspaceTemp + cKelvin))
+  # CH4eqAir <- (eqCH4 * cPresConv * baro * (volGas/1000))/(cGas * (headspaceTemp + cKelvin))
+  # N2OeqAir <- (eqN2O * cPresConv * baro * (volGas/1000))/(cGas * (headspaceTemp + cKelvin))
   # 
-  # #Mol of gas in reference air (using ideal gas law)
-  # CO2air <- (airCO2 * cPresConv * baro * (volGas/1000))/(cGas * (waterTemp + cKelvin))
-  # CH4air <- (airCH4 * cPresConv * baro * (volGas/1000))/(cGas * (waterTemp + cKelvin))
-  # N2Oair <- (airN2O * cPresConv * baro * (volGas/1000))/(cGas * (waterTemp + cKelvin))
+  # #Mol of gas in source gas (using ideal gas law)
+  # CO2air <- (airCO2 * cPresConv * baro * (volGas/1000))/(cGas * (headspaceTemp + cKelvin))
+  # CH4air <- (airCH4 * cPresConv * baro * (volGas/1000))/(cGas * (headspaceTemp + cKelvin))
+  # N2Oair <- (airN2O * cPresConv * baro * (volGas/1000))/(cGas * (headspaceTemp + cKelvin))
   # 
   # #Total mol of gas is sum of equilibrated water and equilibrated air
   # CO2tot <- CO2eqWat + CO2eqAir
--- testingAndDev/jbCalcs/demo.R
+++ testingAndDev/jbCalcs/demo.R
@@ -3,69 +3,97 @@ library(tidyverse)
 library(readxl)
 
 
-
-
 # LOAD FUNCTIONS FROM NEON-dissolved-gas PACKAGE------------
-
 # NEON Function for calculating dissolved gas concentration
 source("neonDissGas/R/def.calc.sdg.R")
 
-# Modified NEON function for calculating dissolved gas concentration
-source("neonDissGas/jbCalcs/def.calc.sdg.jb.R")
-
-# Function for loading and formatting data from NEON database
-source("neonDissGas/R/def.format.sdg.R")
-
-
 
 # LOAD DATA---------------
-# This function calls a function from the 'neonDataStackR' package, which
-# I don't have.  Function fails.
-def.format.sdg(dataDir = paste0(getwd(),"/NEON_dissolved-gases-surfacewater.zip"))
-
-if(file.exists('C:/Users/kcawley/Documents/GitHub/NEON-dissolved-gas')){
-  setwd("C:/Users/kcawley/Documents/GitHub/NEON-dissolved-gas")
-}
-
-
 # Load example data directly from package
 load("neonDissGas/data/sdgFormatted.Rda")
 
+# Add data for expanded function 
+# Define headspace equilibration temperature
+sdgFormatted$headspaceTemp <- sdgFormatted$waterTemp + 3 
+
+# Define concentration in headspace source gas
+# In neon data 'Air' is headspace source gas
+sdgFormatted$concentrationCO2Source <- sdgFormatted$concentrationCO2Air
+sdgFormatted$concentrationCH4Source <- sdgFormatted$concentrationCH4Air
+sdgFormatted$concentrationN2OSource <- sdgFormatted$concentrationN2OAir
+
+# Define concentration in air
+sdgFormatted$concentrationCO2Air <- 405 # global average
+sdgFormatted$concentrationCH4Air <- 1.85 # global average
+sdgFormatted$concentrationN2OAir <- 0.330 # global average
+
+# I separately calculated the dg concentration using an Excel spreadsheet
+# built in 2010.  Load results for comparison here.
+xlData <- read_excel("testingAndDev/jbCalcs/Dissolved gas headspace equilibration June 2010 revision.xls",
+                      sheet = "rExport", na = "NA")
+
+# Merge data
+dissGasData <- cbind(sdgFormatted, xlData)
 
 
 # DISSOLVED GAS CALCULATIONS----------------
 # Calculate dg- NEON
-sdgFormattedNeon <- def.calc.sdg(sdgFormatted) # neon dg calcs
+dissGasRes <- def.calc.sdg(dissGasData) # neon dg calcs
+str(dissGasRes) # as expected
 
-# Calculate dg- Beaulieu function
-sdgFormattedJb <- def.calc.sdg.jb(sdgFormatted) # neon dg calcs
 
-# I separately calculated the dg concentration using an Excel spreadsheet
-# built in 2010.  Load results for comparison here.
-xlCalcs <- read_excel("neonDissGas/jbCalcs/Dissolved gas headspace equilibration June 2010 revision.xls",
-                      sheet = "rExport", na = "NA")
-
-# Merge data
-dissCalcs <- cbind(sdgFormattedNeon, 
-                   select(sdgFormattedJb, dissolvedCO2Jb),
-                   xlCalcs)
 
 # COMPARE CALCULATION RESULTS-----------
-# Neon function vs JB's modified function
-# Good correspondence
-ggplot(dissCalcs, aes(dissolvedCO2, dissolvedCO2Jb)) +
-  geom_point() +
-  geom_abline(slope = 1, intercept = 0) +
-  ylab("Dissolved CO2 derived from JB function (mol l-1)") +
-  xlab("Dissolved CO2 derived from Neon function (mol l-1)")
-
 # Neon function vs JB's Excel calcs
-summarise(dissCalcs, # Excel calcs ~7% greater than Neon
+# Dissolved CO2
+summarise(dissGasRes, # Excel calcs ~7% greater than Neon
           co2Bias = mean(dissolvedCO2.xl / dissolvedCO2, na.rm = TRUE))
 
-ggplot(dissCalcs, aes(dissolvedCO2, dissolvedCO2.xl)) +
+ggplot(dissGasRes, aes(dissolvedCO2, dissolvedCO2.xl)) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0) +
   ylab("Dissolved CO2 derived from JB Excel file (mol l-1)") +
   xlab("Dissolved CO2 derived from Neon function (mol l-1)") +
   ggtitle("Excel calcs ~7% higher than Neon")
+
+# Saturated CO2
+summarise(dissGasRes, # Excel calcs ~1% lower than Neon
+          co2Bias = mean(satCO2.xl / satCO2, na.rm = TRUE))
+
+ggplot(dissGasRes, aes(satCO2, satCO2.xl)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  ylab("Sat CO2 derived from JB Excel file (mol l-1)") +
+  xlab("Sat CO2 derived from Neon function (mol l-1)")
+
+# Dissolved CH4
+summarise(dissGasRes, # Excel calcs ~10% greater than Neon
+          CH4Bias = mean(dissolvedCH4.xl / dissolvedCH4, na.rm = TRUE))
+
+ggplot(dissGasRes, aes(dissolvedCH4, dissolvedCH4.xl)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  ylab("Dissolved CH4 derived from JB Excel file (mol l-1)") +
+  xlab("Dissolved CH4 derived from Neon function (mol l-1)")
+
+# Saturated CH4
+summarise(dissGasRes, # Excel calcs ~8% lower than Neon
+          CH4Bias = mean(satCH4.xl / satCH4, na.rm = TRUE))
+
+ggplot(dissGasRes, aes(satCH4, satCH4.xl)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  ylab("Sat CH4 derived from JB Excel file (mol l-1)") +
+  xlab("Sat CH4 derived from Neon function (mol l-1)")
+
+# TEST IF HEADSPACE TEMP IS NOT PROVIDED-------------------------
+sdgFormatted$headspaceTemp <- NA
+dissGasRes <- def.calc.sdg(dissGasData) # neon dg calcs
+str(dissGasRes) # as expected
+
+# Good, defaults to water body temp if headspace temp not provided.
+ggplot(dissGasRes, aes(dissolvedCO2, dissolvedCO2.xl)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  ylab("Dissolved CO2 derived from JB Excel file (mol l-1)") +
+  xlab("Dissolved CO2 derived from Neon function (mol l-1)")
